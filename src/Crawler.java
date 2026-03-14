820adb62329b92482becfc334099a131c4d35070
Jeje90923 <jeje90923@gmail.com>
2026-03-14 15:52:40 +0800

    Gradle

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,75 +1,34 @@
-package src;
-
+import java.lang.reflect.Array;
 import java.util.Vector;
-import org.htmlparser.beans.StringBean;
-import org.htmlparser.Node;
-import org.htmlparser.NodeFilter;
-import org.htmlparser.Parser;
-import org.htmlparser.filters.AndFilter;
-import org.htmlparser.filters.NodeClassFilter;
-import org.htmlparser.tags.LinkTag;
-import org.htmlparser.util.NodeList;
-import org.htmlparser.util.ParserException;
-import java.util.StringTokenizer;
-import org.htmlparser.beans.LinkBean;
 import java.net.URL;
+import java.util.ArrayList;
 
+import org.jsoup.Connection;
 
 public class Crawler
 {
-	private String url;
+	private String url = "http://www.cs.ust.hk/~dlee/4321/";
+	ArrayList<String> visited;
+
 	Crawler(String _url)
 	{
 		url = _url;
+		visited = new ArrayList<String>();
 	}
-	public Vector<String> extractWords() throws ParserException
-
+	private static void crawl(int level)
 	{
-		// extract words in url and return them
-		// use StringTokenizer to tokenize the result from StringBean
-		// ADD YOUR CODES HERE
-
+		
 	}
-	public Vector<String> extractLinks() throws ParserException
 
-	{
-		// extract links in url and return them
-		// ADD YOUR CODES HERE
-	    
+	private static Document request(String url){
+		try{
+			Connection con = Jsoup.connect(url);
+		}
 	}
 	
 	public static void main (String[] args)
 	{
-		try
-		{
-			Crawler crawler = new Crawler("http://www.cs.ust.hk/~dlee/4321/");
-
-
-			Vector<String> words = crawler.extractWords();		
-			
-			System.out.println("Words in "+crawler.url+" (size = "+words.size()+") :");
-			for(int i = 0; i < words.size(); i++)
-				if(i<5 || i>words.size()-6){
-					System.out.println(words.get(i));
-				} else if(i==5){
-					System.out.println("...");
-				}
-			System.out.println("\n\n");
-			
-
-	
-			Vector<String> links = crawler.extractLinks();
-			System.out.println("Links in "+crawler.url+":");
-			for(int i = 0; i < links.size(); i++)		
-				System.out.println(links.get(i));
-			System.out.println("");
-			
-		}
-		catch (ParserException e)
-            	{
-                	e.printStackTrace ();
-            	}
-
+		// crawl(1);
 	}
 }
 
